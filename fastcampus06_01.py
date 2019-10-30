4ddc3e6923fb61ce4a25a77a4d021822d34e0c80
marinehayong <marinehayong@gmail.com>
2019-10-30 11:43:00 +0900

    git 공부 끝. 빠르게 완강하기

--- fastcampus06_01.py
+++ fastcampus06_01.py
@@ -50,6 +50,98 @@ f.close()
 readline 함수를 통해 텍스트 파일을 한줄 씩 읽는다.
 readline 함수는 더 이상 읽을 줄이 없다면 None을 반환하며,
 조건문을 통해 while문을 탈출
+"""
 
-10:18
+f = open("some.txt", "r")
+data = f.readlines()
+f.close()
+for line in data:
+    print(line, end = "")
+
+"""
+readlines()함수(readline과 다름!!)는 텍스트 파일의 모든 라인을 읽어서
+줄 단위로 요소를 가지는 리스트로 반환합니다.
+그 리스트를 반복문을 통해 읽는 것
+
+open(파일이 존재하는 경로와 파일명, 모드, 인코딩)
+파일을 열땐 모드라는 게 존재한다.
+파일이 열릴 때 작업하고자 하는 모드로 열어줘야 한다.
+당연히 작업하고자 하는 모드와 맞지 않는 작업을 하게되면 에러가 난다.
+
+r 읽기모드
+w 쓰기모드
+x
+a
+...
+"""
+
+# 최소한으로 필기하며 빠르게 공부하기
+
+# 예외처리(Error Handling)
+
+"""
+파이썬에서는 이러한 상황에서 유연하게 대처하고자
+try-except라는 예약어를 제공한다.
+
+try:
+    시도할 코드
+except:
+    에러 발생시 실행할 코드
+
+else도 가능
+fonally문은 try문을 수행 도중 예외의 발생 여부와 상관없이 무조건 수행
+에러가 나도 실행이 안나도 실행. DB connection이나 열린 파일 등을 닫아야 할 때
+close() 해서 리소스를 회수해야 할 때 주로 사용
+
+custom error도 가능
+"""
+
+# git
+
+"""
+리눅스 토발즈가 개발한 분산형 버전 관리 시스템(VCS)
+VCS(Version Control System)란?
+동일한 정보에 대한 여러 버전을 관리하는 프로그래밍 세계에서는 소스코드
+
+토발즈는 버전 관리 시스템으로 Bitkeeper를 쓰며 리눅스를 개발하고 있다가
+여러 이념들의 충돌과 성능에 화가나서 2주만에 버전 관리 시스템 Git을 만들어버림ㅋ
+
+git의 특징
+빠른 속도, 단순한 구조
+분산형 저장소 지원
+비선형적 개발(브랜치를 나누어 개발) 가능
+
+코드 백업
+저장소를 통한 협업
+
+소스코드를 주고 받지 않아도 동시작업이 가능 -> 생산성 증가
+수정내용을 commit 단위로 관리, 원하는 시점으로 돌아가기 가능 -> 백업
+branch 단위로 개발하여 A를 개발하던 도중 B라는 기능 추가 가능 -> 편안한 테스트 가능
+인터넷이 없어도 개발이 가능 -> 장소제약이 없어짐
+
+git config --global user.username ""
+git config --list
+git remote add origin https://~
+git push -u origin master
+
+reset
+revert
+log
+status
+
+branch란?
+분기점을 생성하고 동일한 소스코드에서 다른 개발을 하기 위한 활동
+git은 처음 세팅을 할 때 master라는 branch를 기본값으로 만들어 줍니다.
+
+git branch <branch이름>
+git checkout <branch이름>
+
+merge: 두개의 branch의 코드를 합쳐주는 기능
+git merge <합칠branch>
+merge를 실행하면 현재 branch에 합쳐진다.
+
+git checkout master
+git merge login_logic
+
+conflict
 """
