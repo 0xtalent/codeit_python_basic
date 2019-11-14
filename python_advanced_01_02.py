a411a6dd5fd9f489296d442cf1c63fba8758c2a5
marinehayong <marinehayong@gmail.com>
2019-11-14 22:05:57 +0900

    클래스 공부 계속, 개발자 꼭 될거야! 힘차게 노력하자!!

--- python_advanced_01_02.py
+++ python_advanced_01_02.py
@@ -37,3 +37,37 @@ class Student():
 
     def __del__(self):
         Student.student_count -= 1
+
+# Self 의미
+studt1 = Student('Cho', 2, 3, {'gender': 'Male', 'score1': 65, 'score2': 44})
+studt2 = Student('Chang', 4, 1, {'gender': 'Female', 'score1': 85, 'score2': 74}, 'superhayong@naver.com')
+
+# ID 확인
+print(id(studt1))
+print(id(studt2))
+
+# dir & __dict__ 확인
+
+print(dir(studt1))
+print(dir(studt2))
+
+print()
+print()
+
+print(studt1.__dict__)
+print(studt2.__dict__)
+
+# Docstring
+
+print(Student.__doc__)
+print()
+
+# 실행
+studt1.detail_info()
+studt2.detail_info()
+
+"""
+클래스는 속성과 메소드로 이루어진다.
+속성에서 인스턴스 변수는 내꺼
+클래스 변수는 모두가 공유한다
+"""
