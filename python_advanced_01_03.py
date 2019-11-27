f23471a22cfc0631f398a7afe85f181b9d7f5649
marinehayong <marinehayong@gmail.com>
2019-11-27 17:13:59 +0900

    모든 인스턴스가 공통으로 접근하는 클래스 객체를 활용해서 사용하는 메소드들은 클래스 데코레이터들을 붙여준다음에 메소드를 정의하면 된다. 첫번째 인자로는 클래스 자체가 넘어온다

--- python_advanced_01_03.py
+++ python_advanced_01_03.py
@@ -2,6 +2,12 @@
 # 파이썬 심화
 # 클래스 메소드, 인스턴스 메소드, 스테이틱 메소드
 
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
 # 기본 인스턴스 메소드
 
 class Student(object):
@@ -39,8 +45,19 @@ class Student(object):
     def get_fee_culc(self):
         return 'After Tuition -> ID : {}, fee : {}'.format(self._id, self._tuition * Student.tuition_per)
 
+    # Instance Method
     def __str__(self):
         return 'Student Info -> name: {} grade: {} email: {}'.format(self.full_name(), self._grade, self._email)
+
+    # Class Method
+    @classmethod
+    def raise_fee(cls, per):
+        if per <= 1:
+            print("더 큰 수를 입력해라")
+            return
+        cls.tuition_per = per
+        print("인상되었습니다")
+
 
 # 학생 인스턴스
 student_1 = Student(1, 'Kim', 'Sarang', 'student1@naver.com', '1', 400, 3.5)
@@ -61,7 +78,9 @@ print(student_2.get_fee())
 
 print()
 # 학비 인상(클래스 메소드 미사용)
-Student.tuition_per = 1.2
+# Student.tuition_per = 1.2
+
+Student.raise_fee(1.2)
 
 # 학비 정보(등록금 인상 후)
 print(student_1.get_fee_culc())
