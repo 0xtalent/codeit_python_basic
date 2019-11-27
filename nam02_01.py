7bd5d7960d61fea5211c418f80cfaa91f34011d2
marinehayong <marinehayong@gmail.com>
2019-11-27 20:20:41 +0900

    오늘 프로그래밍 공부 여기까지

--- nam02_01.py
+++ nam02_01.py
@@ -35,3 +35,5 @@ for i in range(2, 10):
 
 gugudan = ["{} x {} = {}".format(i, j, i * j) for i in range(2, 10) for j in range(1, 10)]
 print(gugudan)
+
+# 
--- python_advanced_01_03.py
+++ python_advanced_01_03.py
@@ -63,6 +63,13 @@ class Student(object):
     def student_const(cls, id, first_name, last_name, email, grade, tuition, gpa):
         return cls(id, first_name, last_name, email, grade, tuition * cls.tuition_per, gpa)
 
+    # Static Method
+    @staticmethod
+    def is_scholarship_st(inst):
+        if inst._gpa >= 4.3:
+            return '{} 은(는) 장학금 대상자 입니다.'.format(inst._last_name)
+        return '장학금 대상자가 아닙니다.'
+
 # 학생 인스턴스
 student_1 = Student(1, 'Kim', 'Sarang', 'student1@naver.com', '1', 400, 3.5)
 student_2 = Student(2, 'Lee', 'Myungho', 'student2@naver.com', '2', 500, 4.3)
@@ -92,8 +99,8 @@ print(student_2.get_fee_culc())
 print()
 
 # 클래스 메소드 인스턴스 생성 실습
-student_3 = Student.student_const(3, 'Park', 'Minji', 'Student3@gmail.com', '3', 550, 4.5)
-student_4 = Student.student_const(4, 'Jung', 'Hayong', 'superhayong@naver.com', '4', 600, 4.1)
+student_3 = Student.student_const(3, 'Park', 'Minji', 'Student3@gmail.com', '3', 550, 3.8)
+student_4 = Student.student_const(4, 'Jung', 'Hayong', 'superhayong@naver.com', '4', 600, 4.5)
 
 # 전체 정보 출력
 print(student_3.detail_info())
@@ -103,3 +110,29 @@ print()
 # 학생 학비 변경 확인
 print(student_3._tuition)
 print(student_4._tuition)
+print()
+
+# 장학금 혜택 여부 알아보기(스테이틱 메소드 미사용)
+def is_scholarship(inst):
+    if inst._gpa >= 4.3:
+        return '{} 은(는) 장학금 대상자 입니다.'.format(inst._last_name)
+    return '장학금 대상자가 아닙니다.'
+
+print(is_scholarship(student_1))
+print(is_scholarship(student_2))
+print(is_scholarship(student_3))
+print(is_scholarship(student_4))
+print()
+
+# 장학금 혜택 여부 알아보기(스테이틱 메소드 사용)
+
+print(Student.is_scholarship_st(student_1))
+print(Student.is_scholarship_st(student_2))
+print(Student.is_scholarship_st(student_3))
+print(Student.is_scholarship_st(student_4))
+print()
+
+print(student_1.is_scholarship_st(student_1))
+print(student_2.is_scholarship_st(student_2))
+print(student_3.is_scholarship_st(student_3))
+print(student_4.is_scholarship_st(student_4))
