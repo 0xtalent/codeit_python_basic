3454263500453c1f74cba23eab1303d476ab1d6c
marinehayong <marinehayong@gmail.com>
2019-11-28 19:58:56 +0900

    파이썬 심화 매직 메소드 공부 끝

--- python_advanced_02_02.py
+++ python_advanced_02_02.py
@@ -69,3 +69,44 @@ print("EX2-4 -", s2 - s1)
 print("EX2-5 -", s1)
 print("EX2-6 -", s2)
 print()
+
+# 클래스 예제2
+
+class Vector(object):
+    def __init__(self, *args):
+        """Create a vector, example : v = vector(1, 2)"""
+        if len(args) == 0:
+            self._x, self._y = 0, 0
+        else:
+            self._x, self._y = args
+
+    def __repr__(self):
+        """Retruns the vector informations"""
+        return 'Vector(%r, %r)' % (self._x, self._y)
+
+    def __add__(self, other):
+        """Returns the vector addition of self and other"""
+        return Vector(self._x + other._x, self._y + other._y)
+
+    def __mul__(self, y):
+        return Vector(self._x * y, self._y * y)
+
+    def __bool__(self):
+        return bool(max(self._x, self._y))
+
+# Vector 인스턴스 생성
+v1 = Vector(3, 5)
+v2 = Vector(15, 20)
+v3 = Vector()
+
+# 매직메소드 출력
+print("EX3-1 -", Vector.__init__.__doc__)
+print("EX3-2 -", Vector.__repr__.__doc__)
+print("EX3-3 -", Vector.__add__.__doc__)
+print("EX3-4 -", v1, v2, v3)
+print("EX3-5 -", v1 + v2)
+print("EX3-6 -", v1 * 4)
+print("EX3-7 -", v2 * 10)
+print("EX3-8 -", bool(v1), bool(v2))
+print("EX3-9 -", bool(v3))
+print()
