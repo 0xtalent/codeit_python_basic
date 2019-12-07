a64cc8bacad1488a55da91b2d1255189ef095c46
marinehayong <marinehayong@gmail.com>
2019-12-07 22:08:39 +0900

    전체를 함수로 만들버리기

--- 남박사 파이썬 실전 프로그래밍/04. 계산기 만들기/smart.py
+++ 남박사 파이썬 실전 프로그래밍/04. 계산기 만들기/smart.py
@@ -3,35 +3,49 @@
 
 # 콘솔 스마트 계산기 만들기 스마트-파이썬 기초, 계산기 로직, 리스트, 반복문
 
-user_input = input("계산식을 입력하세요> ")
 opertor = ["+", "-", "*", "/", "="]
 
-string_list = []
-lop = 0
+def string_caculator(user_input):
+    string_list = []
+    lop = 0
 
-# 마지막에 operator로 안끝나면 임의로 =을 추가해주자
-if user_input[-1] not in opertor:
-    user_input += "="
+    # 마지막에 operator로 안끝나면 임의로 =을 추가해주자
+    if user_input[-1] not in opertor:
+        user_input += "="
 
-# 리스트에 몇번째에 있는지도 알아야 하니까 enumerate로 감싸서
-for i, s in enumerate(user_input):
-    if s in opertor:
-        if user_input[lop:i].strip != "":
-            string_list.append(user_input[lop:i])
-            string_list.append(s)
-            lop = i + 1
+    # 리스트에 몇번째에 있는지도 알아야 하니까 enumerate로 감싸서
+    for i, s in enumerate(user_input):
+        if s in opertor:
+            if user_input[lop:i].strip != "":
+                string_list.append(user_input[lop:i])
+                string_list.append(s)
+                lop = i + 1
 
-# =은 임의로 넣은 거니까 마지막꺼 빼고 할당
-string_list = string_list[:-1]
+    # =은 임의로 넣은 거니까 마지막꺼 빼고 할당
+    string_list = string_list[:-1]
 
-pos = 0
-while True:
-    if pos + 1 > len(string_list):
-        break
-    if len(string_list) > pos + 1 and string_list[pos] in opertor:
-        temp = string_list[pos-1] + string_list[pos] + string_list[pos+1]
-        del string_list[0:3]
-        string_list.insert(0, str(eval(temp)))
-        pos = 0
-    pos += 1
-    print(string_list)
+    # ['10', '+', '10', '+', '10', '+', '10']
+    # ['10', '+', '10', '+', '10', '+', '10']
+    # ['20', '+', '10', '+', '10']
+    # ['30', '+', '10']
+    # ['40']
+
+    pos = 0
+    while True:
+        if pos + 1 > len(string_list):
+            break
+        if len(string_list) > pos + 1 and string_list[pos] in opertor:
+            temp = string_list[pos-1] + string_list[pos] + string_list[pos+1]
+            del string_list[0:3]
+            string_list.insert(0, str(eval(temp)))
+            pos = 0
+        pos += 1
+
+    if len(string_list) > 0:
+        result = float(string_list[0])
+
+    return round(result, 4)
+
+user_input = input("계산식을 입력하세요> ")
+result = string_caculator(user_input)
+print("결과: {}".format(result))
