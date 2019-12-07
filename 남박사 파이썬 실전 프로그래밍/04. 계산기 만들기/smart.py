ade2a718ffd10a1fc706065da1f439e0ad4a3320
marinehayong <marinehayong@gmail.com>
2019-12-07 21:34:32 +0900

    마지막 값 안들어가는거 수정

--- 남박사 파이썬 실전 프로그래밍/04. 계산기 만들기/smart.py
+++ 남박사 파이썬 실전 프로그래밍/04. 계산기 만들기/smart.py
@@ -9,6 +9,10 @@ user_input = "5656 + 51212 * 10"
 string_list = []
 lop = 0
 
+# 마지막에 operator로 안끝나면 임의로 =을 추가해주자
+if user_input[-1] not in opertor:
+    user_input += "="
+
 # 리스트에 몇번째에 있는지도 알아야 하니까 enumerate로 감싸서
 for i, s in enumerate(user_input):
     if s in opertor:
@@ -16,4 +20,7 @@ for i, s in enumerate(user_input):
             string_list.append(user_input[lop:i])
             string_list.append(s)
             lop = i + 1
+
+# =은 임의로 넣은 거니까 마지막꺼 빼고 할당
+string_list = string_list[:-1]
 print(string_list)
