b07ad1e743a5640e6a71a2589aa94095ab9bd49a
marinehayong <marinehayong@gmail.com>
2019-12-08 19:45:10 +0900

    네이버 영화 평점 크롤링 할라는데 이상하게 업데이트해서 보기만 할게요

---
+++ 남박사 파이썬 실전 프로그래밍/09. 크롤링/naver_movie.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://movie.naver.com/movie/point/af/list.nhn?&page=1"
+r = requests.get(url)
+bs = BeautifulSoup(r.text, "lxml")
+
+trs = bs.select("table.list_netizen > tbody > tr")
+for tr in trs:
+    tds = tr.select("td")
+    if len(tds) != 5:
+        continue
