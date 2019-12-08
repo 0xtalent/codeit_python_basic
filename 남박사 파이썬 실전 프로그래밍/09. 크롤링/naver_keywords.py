34f1deb601ff68ba078d582d263ded03c3cedfbf
marinehayong <marinehayong@gmail.com>
2019-12-08 12:39:08 +0900

    select로 네이버 실시간 검색어 긁어오기

--- 남박사 파이썬 실전 프로그래밍/09. 크롤링/naver_keywords.py
+++ 남박사 파이썬 실전 프로그래밍/09. 크롤링/naver_keywords.py
@@ -4,8 +4,13 @@ from bs4 import BeautifulSoup
 r = requests.get("https://www.naver.com")
 bs = BeautifulSoup(r.text, "html.parser")
 
-lists = bs.find_all("li", {"class": "ah_item"})
+# lists = bs.find_all("li", {"class": "ah_item"})
 
+# for li in lists:
+#     title = li.find("span", {"class": "ah_k"}).text
+#     print(title)
+
+lists = bs.select("li.ah_item")
 for li in lists:
-    title = li.find("span", {"class": "ah_k"}).text
+    title = li.select("span.ah_k")[0].text
     print(title)
