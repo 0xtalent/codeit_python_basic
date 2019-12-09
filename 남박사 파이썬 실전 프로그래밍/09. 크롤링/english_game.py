a57e5a2d2a78f1f0caa2df57c8b7322f1839ec5a
marinehayong <marinehayong@gmail.com>
2019-12-09 11:32:26 +0900

    for문 안 돌고 기사 하나만 크롤링, 리턴 하면 그냥 리턴값만 반환하고 끝인가벼?

--- 남박사 파이썬 실전 프로그래밍/09. 크롤링/english_game.py
+++ 남박사 파이썬 실전 프로그래밍/09. 크롤링/english_game.py
@@ -1,20 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.usatoday.com"
-r = requests.get(url)
-bs = BeautifulSoup(r.text, "lxml")
-lists = bs.select("div.gnt_m_th > a")
-for li in lists:
-    href = url + li["href"]
-    print(href)
-
-    r = requests.get(href)
+def get_news():
+    url = "https://www.usatoday.com"
+    r = requests.get(url)
     bs = BeautifulSoup(r.text, "lxml")
-    texts = bs.select("div.gnt_ar_b > p.gnt_ar_b_p")
-    contents = [p.text for p in texts]
-    # 리스트 컴프리헨션
-    # contents = []
-    # for p in texts:
-    #     contents.append(p.text)
-    print(contents)
+    lists = bs.select("div.gnt_m_th > a")
+    for li in lists:
+        href = url + li["href"]
+        print(href)
+
+        r = requests.get(href)
+        bs = BeautifulSoup(r.text, "lxml")
+        texts = bs.select("div.gnt_ar_b > p.gnt_ar_b_p")
+        contents = [p.text for p in texts]
+        # 리스트 컴프리헨션
+        # contents = []
+        # for p in texts:
+        #     contents.append(p.text)
+        contents = " ".join(contents)
+        # 이렇게 하면 for문을 안 돈다는 데 이거 이해 안가네
+        return contents.lower()
+    return None
+
+print(get_news())
