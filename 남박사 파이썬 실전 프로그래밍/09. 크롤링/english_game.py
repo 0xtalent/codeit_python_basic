908fa28fb93bf5d7d98e2d57a5766594131da3ef
marinehayong <marinehayong@gmail.com>
2019-12-09 11:06:01 +0900

    ﻿

---
+++ 남박사 파이썬 실전 프로그래밍/09. 크롤링/english_game.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.usatoday.com/"
+r = requests.get(url)
+bs = BeautifulSoup(r.text, "lxml")
+lists = bs.select("div.gnt_m_th > a")
+for li in lists:
+    print(li)
