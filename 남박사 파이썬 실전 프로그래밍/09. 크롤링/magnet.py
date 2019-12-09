4dc922c23339767c6028c495e48aa6200247ce9f
marinehayong <marinehayong@gmail.com>
2019-12-09 16:38:49 +0900

    흠 구현은 했는데 첫페이지부터 마그넷 주소 크롤링 안됨 그냥 넘어갈게요

--- 남박사 파이썬 실전 프로그래밍/09. 크롤링/magnet.py
+++ 남박사 파이썬 실전 프로그래밍/09. 크롤링/magnet.py
@@ -2,19 +2,47 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-keyword = "리눅스"
-url = "https://www.google.com/search?q={0}+magnet%3A%3Fxt%3D&rlz=1C1SQJL_koKR871KR871&oq={0}+magnet%3A%3Fxt%3D".format(keyword)
+def search_google(keyword, start_page=10, end_page=None):
+    url = "https://www.google.com/search?q={0}+magnet%3A%3Fxt%3D&rlz=1C1SQJL_koKR871KR871&oq={0}+magnet%3A%3Fxt%3D".format(keyword)
 
-header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"}
-r = requests.get(url, headers=header)
-bs = BeautifulSoup(r.text, "lxml")
-links = bs.select("div.g > div > div.rc > div.r > a")
+    header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"}
+    r = requests.get(url, headers=header)
+    bs = BeautifulSoup(r.text, "lxml")
+    links = bs.select("div.g > div > div.rc > div.r > a")
 
-for a in links:
-    href = a["href"]
-    text = a.select("h3")
-    if len(text) <= 0:
-        continue
-    title = text[0].text
-    print(title)
-    
+    results = []
+
+    if end_page is None:
+        counts = bs.select("div#resultStats")[0].text.replace("검색결과 약", "").replace("개", "").replace(",", "").split("(")[0].strip()
+        end_page = int(int(counts) / 10)
+        if end_page > 20:
+            end_page = 20
+
+    for a in links:
+        href = a["href"]
+        text = a.select("h3")
+        if len(text) <= 0:
+            continue
+        title = text[0].text
+        
+        try:
+            r = requests.get(href)
+            bs = BeautifulSoup(r.text, "lxml")
+            magnets = bs.find_all("a", href=re.compile(r'magnet:\?xt=*'))
+
+            if len(magnets) > 0:
+                magnet = magnets[0]["href"]
+                results.append(title, manget)
+        except:
+            pass
+
+    if start_page < end_page:
+        start_page += 10
+        results.extend(search_google(keyword, start_page, end_page=end_page))
+
+    return results
+        
+results = search_google("리눅스", 0)
+
+for r in results:
+    print(r)
