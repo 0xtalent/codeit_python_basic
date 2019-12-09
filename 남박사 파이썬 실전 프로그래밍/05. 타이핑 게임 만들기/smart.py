f95b9519451aa0dc6bdf3b34d0594f4629e5cbf9
marinehayong <marinehayong@gmail.com>
2019-12-09 21:44:11 +0900

    전 심플 문제와 합해서 스마트 문제로 변형

--- 남박사 파이썬 실전 프로그래밍/05. 타이핑 게임 만들기/smart.py
+++ 남박사 파이썬 실전 프로그래밍/05. 타이핑 게임 만들기/smart.py
@@ -6,6 +6,10 @@
 종성 = (x - 44032) % 28
 '''
 
+import time
+import random
+import os
+
 CHO = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅛ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
 JUNG = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"]
 JONG = ["", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "뵤", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
@@ -17,32 +21,64 @@ JONG = ["", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ"
 # a = chr(((0 * 21) + 0) * 28 + 0 + 44032)
 # print(a)
 
-user_input = input("입력> ")
-word_list = list(user_input)
+def break_korean(string):
+    word_list = list(string)
+    break_word = []
 
-break_word = []
+    for k in word_list:
+        if ord(k) >= ord("가") and ord(k) <= ord("힣"):
+            # 유니코드 상 몇번째 글자인지 인덱스를 구한다.
+            char_index = ord(k) - ord('가')
 
-for k in word_list:
-    if ord(k) >= ord("가") and ord(k) <= ord("힣"):
-        # 유니코드 상 몇번째 글자인지 인덱스를 구한다.
-        char_index = ord(k) - ord('가')
+            # 초성 = ((문자코드 - 44032) / 28) / 21
+            char1 = int((char_index / 28) / 21)
+            break_word.append(CHO[char1])
 
-        # 초성 = ((문자코드 - 44032) / 28) / 21
-        char1 = int((char_index / 28) / 21)
-        break_word.append(CHO[char1])
+            # 중성 = ((x - 44032) / 28) % 21
+            char2 = int((char_index / 28) % 21)
+            break_word.append(JUNG[char2])
 
-        # 중성 = ((x - 44032) / 28) % 21
-        char2 = int((char_index / 28) % 21)
-        break_word.append(JUNG[char2])
+            # 종성 = (x - 44032) % 28
+            char3 = int(char_index % 28)
 
-        # 종성 = (x - 44032) % 28
-        char3 = int(char_index % 28)
+            if char3 > 0:
+                break_word.append(JONG[char3])
+        
+        else:
+            break_word.append(k)
+    return break_word
 
-        if char3 > 0:
-            break_word.append(JONG[char3])
-    
-    else:
-        break_word.append(k)
+word_list = [
+    "2019년 EBS에서 제작한 유튜브 채널 자이언트 펭TV의 주인공으로 남극에서 온 펭귄이다.",
+    "'자이언트 펭TV'는 평일 저녁 어린이 예능인 생방송 톡!톡! 보니하니의 한 코너였으나",
+    "EBS는 2019년 가을 개편에서 '자이언트 펭TV'를 별도 프로그램으로 독립시켜",
+    "금요일 저녁 8시 30분에 편성했다."
+]
 
-print("입력: {}".format(user_input))
-print("분해: {}".format(break_word))
+random.shuffle(word_list)
+
+for q in word_list:
+    os.system("cls")
+    start_time = time.time()
+    user_input = str(input(q + '\n')).strip()
+    end_time = time.time() - start_time
+
+    src = break_korean(q)
+    tar = break_korean(user_input)
+
+    if user_input == "/exit":
+        break
+
+    correct = 0
+    for i, c in enumerate(tar):
+        if i >= len(src):
+            break
+        if c == src[i]:
+            correct += 1
+    tot_len = len(q)
+    c = correct / tot_len * 100
+    e = (tot_len - correct) / tot_len * 100
+    speed = (correct / end_time) * 60
+
+    print("속도: {:0.2f} 정확도: {} 오타율: {}".format(speed, c, e))
+    os.system("pause")
