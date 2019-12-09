a8dd7e0b0efa6da2be77552ee8731e263f5faacd
marinehayong <marinehayong@gmail.com>
2019-12-09 16:11:43 +0900

    안들어가지는 페이지가 있어서 제목 긁어오기만 하고 강의볼게요

--- 남박사 파이썬 실전 프로그래밍/09. 크롤링/magnet.py
+++ 남박사 파이썬 실전 프로그래밍/09. 크롤링/magnet.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 keyword = "리눅스"
 url = "https://www.google.com/search?q={0}+magnet%3A%3Fxt%3D&rlz=1C1SQJL_koKR871KR871&oq={0}+magnet%3A%3Fxt%3D".format(keyword)
@@ -7,5 +8,13 @@ url = "https://www.google.com/search?q={0}+magnet%3A%3Fxt%3D&rlz=1C1SQJL_koKR871
 header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"}
 r = requests.get(url, headers=header)
 bs = BeautifulSoup(r.text, "lxml")
-divs = bs.select("div.g")
-print(divs)
+links = bs.select("div.g > div > div.rc > div.r > a")
+
+for a in links:
+    href = a["href"]
+    text = a.select("h3")
+    if len(text) <= 0:
+        continue
+    title = text[0].text
+    print(title)
+    
