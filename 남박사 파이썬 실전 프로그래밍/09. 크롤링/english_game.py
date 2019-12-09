511798dc3b149ca0bcf2f41dced9921991d3831e
marinehayong <marinehayong@gmail.com>
2019-12-09 11:26:07 +0900

    리스트 컴프리헨션으로 텍스트만 가져오기

--- 남박사 파이썬 실전 프로그래밍/09. 크롤링/english_game.py
+++ 남박사 파이썬 실전 프로그래밍/09. 크롤링/english_game.py
@@ -1,10 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.usatoday.com/"
+url = "https://www.usatoday.com"
 r = requests.get(url)
 bs = BeautifulSoup(r.text, "lxml")
 lists = bs.select("div.gnt_m_th > a")
 for li in lists:
-    href = li["href"]
+    href = url + li["href"]
     print(href)
+
+    r = requests.get(href)
+    bs = BeautifulSoup(r.text, "lxml")
+    texts = bs.select("div.gnt_ar_b > p.gnt_ar_b_p")
+    contents = [p.text for p in texts]
+    # 리스트 컴프리헨션
+    # contents = []
+    # for p in texts:
+    #     contents.append(p.text)
+    print(contents)
