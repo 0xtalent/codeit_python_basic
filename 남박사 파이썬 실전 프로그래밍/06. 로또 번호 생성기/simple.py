ee6de1752a066a49c6e329d24cc7832c4a3ae14c
marinehayong <marinehayong@gmail.com>
2019-12-10 09:37:57 +0900

    로또 번호 여러개 만들어주도록 수정

--- 남박사 파이썬 실전 프로그래밍/06. 로또 번호 생성기/simple.py
+++ 남박사 파이썬 실전 프로그래밍/06. 로또 번호 생성기/simple.py
@@ -1,14 +1,16 @@
 import random
 
-lotto = []
+count = int(input("로또 번호 몇개 만들어줄까요?>"))
+for j in range(1, count + 1):
+    lotto = []
 
-rand_num = random.randint(1, 46)
+    rand_num = random.randint(1, 46)
 
-for i in range(6):
-    while rand_num in lotto:
-        rand_num = random.randint(1, 46)
-    lotto.append(rand_num)
+    for i in range(6):
+        while rand_num in lotto:
+            rand_num = random.randint(1, 46)
+        lotto.append(rand_num)
 
-lotto.sort()
+    lotto.sort()
 
-print("로또 번호: {}".format(lotto))
+    print("{}. 로또 번호: {}".format(j, lotto))
