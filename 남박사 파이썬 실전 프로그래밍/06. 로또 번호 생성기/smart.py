525eeb7398c924595c3f91ea403a3a55a6185b55
marinehayong <marinehayong@gmail.com>
2019-12-10 11:14:25 +0900

    로또 스마트 만드는 중간 과정

--- 남박사 파이썬 실전 프로그래밍/06. 로또 번호 생성기/smart.py
+++ 남박사 파이썬 실전 프로그래밍/06. 로또 번호 생성기/smart.py
@@ -11,10 +11,24 @@ def make_lotto_number(**kwargs):
     rand_number = numpy.random.choice(range(1, 46), 6, replace=False)
     rand_number.sort()
 
+    # 최종 로또 번호가 완성될 변수
     lotto = []
 
     # **kwargs 키워드 인자로 파라미터를 준 이유는 사용자가 옵션을 줄수도 있고 안줄수도 있어서
     if kwargs.get("include"):
         include = kwargs.get("include")
+        # extend 시켜줘야 확장됨 append 쓰면 [[1, 2, 3]] 됨
+        lotto.extend(include)
+
+        cnt_make = 6 - len(lotto)
+
+        for _ in range(cnt_make):
+            for j in rand_number:
+                if lotto.count(j) == 0:
+                    lotto.append(j)
+                    break
+
+    else:
+        lotto.extend(rand_number)
 
 make_lotto_number(include=[1, 2])
