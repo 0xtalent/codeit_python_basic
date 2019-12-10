4f6da6fd65e78ffee044517a2ce7de1a17cb9ddd
marinehayong <marinehayong@gmail.com>
2019-12-10 20:03:07 +0900

    단축키 눌리면 특정 함수 호출하게 구현

---
+++ 남박사 파이썬 실전 프로그래밍/08. 나만의 단축키 만들기/smart.py
@@ -0,0 +1,35 @@
+from pynput.keyboard import Key, Listener, KeyCode
+
+MY_HOT_KEYS = [
+    {"function1": {Key.ctrl_l, Key.alt_l, KeyCode(char="n")}}
+]
+
+current_keys = set()
+
+def function1():
+    print("함수1 호출")
+
+def key_pressed(key):
+    print("Pressed {}".format(key))
+    for data in MY_HOT_KEYS:
+        FUNCTION = list(data.keys())[0]
+        KEYS = list(data.values())[0]
+
+        if key in KEYS:
+            current_keys.add(key)
+
+            if all(k in current_keys for k in KEYS):
+                function = eval(FUNCTION)
+                function()
+
+def key_released(key):
+    print("Realesed {}".format(key))
+
+    if key in current_keys:
+        current_keys.remove(key)
+
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=key_pressed, on_release=key_released) as Listener:
+    Listener.join()
