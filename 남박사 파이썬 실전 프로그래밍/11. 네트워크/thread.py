0ee01f4e06c87d78d6e291ad8b0f4b7fd02d55c2
marinehayong <marinehayong@gmail.com>
2020-03-03 14:29:06 +0900

    믹스인 공부

--- 남박사 파이썬 실전 프로그래밍/11. 네트워크/thread.py
+++ 남박사 파이썬 실전 프로그래밍/11. 네트워크/thread.py
@@ -16,5 +16,10 @@ def 우편발송():
 th1 = threading.Thread(target=주문받기)
 th2 = threading.Thread(target=우편발송)
 
+th1.daemon = True
+th2.daemon = True
+
 th1.start()
 th2.start()
+
+# OS가 국가라 가정하면, 프로세스는 공장이고, 쓰레드는 일꾼이다.
